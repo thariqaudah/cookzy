ba21c6047be2f913ab30703157fb9655a47bd086
Thariq Audah <thariq.audah@gmail.com>
2022-03-11 16:39:39 +0700

    Edit footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,7 +2,7 @@
 	<footer class="footer">
 		<div class="container">
 			<p class="footer-text">
-				Built and designed with
+				Built with
 				<font-awesome-icon class="footer-icon" icon="heart" />
 				by
 				<a
@@ -12,13 +12,24 @@
 					>Thariq Audah</a
 				>
 			</p>
+			<a
+				class="footer-github"
+				href="https://github.com/thariqaudah"
+				target="_blank"
+			>
+				<img
+					class="icon-github"
+					src="@/assets/icon-github.svg"
+					alt="Github icon"
+				/>
+			</a>
 		</div>
 	</footer>
 </template>
 
 <style lang="scss" scoped>
 .footer {
-	padding: 96px 0 32px;
+	padding: 96px 0 16px;
 	text-align: center;
 
 	.footer-text {
@@ -29,7 +40,8 @@
 		}
 
 		.footer-link {
-			text-decoration: underline;
+			line-height: 1.2;
+			border-bottom: 1px solid currentColor;
 
 			&:hover {
 				color: var(--primary-color);
@@ -39,6 +51,13 @@
 
 	.footer-icon {
 		color: #e94646;
+	}
+
+	.footer-github {
+		margin-top: 16px;
+		.icon-github {
+			width: 32px;
+		}
 	}
 }
 </style>
